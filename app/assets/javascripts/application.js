d437cd985573e234bb2ade430209da1d07d2d711
krewskatee <lchira99@gmail.com>
2017-10-22 22:11:08 -0500

    added header to posts index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,11 @@
 //= require popper
 //= require bootstrap
 //= require_tree .
+
+$(document).ready(function(){
+  setTimeout(function(){
+    $('.alert').fadeOut("slow", function(){
+      $(this).remove();
+    })
+  }, 1500);
+});
