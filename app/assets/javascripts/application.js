f51c017f0fa2b3460888371fe23936a1e0a8eeb8
krewskatee <lchira99@gmail.com>
2017-10-24 16:24:08 -0500

    added js to comment editing/update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,11 +18,3 @@
 //= require bootstrap
 //= require local-time
 //= require_tree .
-
-$(document).ready(function(){
-  setTimeout(function(){
-    $('.alert').fadeOut("slow", function(){
-      $(this).remove();
-    })
-  }, 1500);
-});
